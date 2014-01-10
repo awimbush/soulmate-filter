0632d53d582d115430a32a31f523f75cfb02de94
Alex Wimbush <alex@yieldpop.com>
2014-01-10 15:05:13 +0100

    undo take out whitesapces on normalize, as it breaks the prefixes_for_phrases

--- lib/soulmate/helpers.rb
+++ lib/soulmate/helpers.rb
@@ -13,8 +13,8 @@ module Soulmate
     end
 
     def normalize(str)
-      # Letter, Mark, Number, Connector_Punctuation (Chinese, Japanese, etc.), spaces
-      str.downcase.gsub(/[^\p{Word}\ ]|\s+/i, '')
+      # Letter, Mark, Number, Connector_Punctuation (Chinese, Japanese, etc.)
+      str.downcase.gsub(/[^\p{Word}\ ]/i, '').strip
     end
 
     def filter_key(dimension, value)
